9aef6e0b67e74a84c0bd99ecd7eb1823fca322d4
puddingdream <edcrfv098@naver.com>
2026-01-21 11:03:35 +0900

    ✨feat : Step 2 계산기클래스 만들고 메인에서 입력기능 추가

--- src/step2/App.java
+++ src/step2/App.java
@@ -1,7 +1,21 @@
 package step2;
 
+import java.util.Scanner;
+
 public class App {
     static void main() {
+        Scanner sc = new Scanner(System.in);
+        int num1 = 0;
+        int num2 = 0;
+        String end = "";
+
+
+        System.out.println("첫 번째 숫자를 입력하세요");
+        num1 = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요");
+        num2 = sc.nextInt();
+
+
 
     }
 }
---
+++ src/step2/Calculator.java
@@ -0,0 +1,25 @@
+package step2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private List<Integer> results = new ArrayList<>();
+
+    private int num1;
+    private int num2;
+    private char oper;
+
+    public Calculator( int num1, int num2, char oper) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.oper = oper;
+    }
+
+    public int calculator(int num1, int num2, char oper){
+
+
+    }
+
+
+}
